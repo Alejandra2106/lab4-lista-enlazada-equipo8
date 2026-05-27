eb4b98d232c2059edbdc95fddad21843560028c8
Isabella <isabellasanchezmejia11@gmail.com>
2026-05-27 18:53:37 -0500

    Código implementado con sugerencias

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -31,7 +31,7 @@ def test_node_repr():
 # Pruebas Equipo C — search
 # ------------------------------------------------------------------ #
 
-def test_search_un_elemento():
+def test_search_elemento_existente():
     ll = LinkedList()
 
     ll.head = Node(10)
